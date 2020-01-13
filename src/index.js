3b846ae1bf38b2a9c7f0934943477c4c420fe1e0
Quint <quint@mx3d.com>
2020-01-13 11:49:54 +0100

    Generate table with loops

--- src/index.js
+++ src/index.js
@@ -20,24 +20,23 @@ class Board extends React.Component {
     );
   }
 
+  createTable = () => {
+    let boards = [];
+
+    for (let i = 0; i < 3; i++) {
+      let childeren = []
+      for (let j = 0; j < 3; j++) {
+        childeren.push(this.renderSquare(i * 3 + j))
+      }
+      boards.push(<div className="board-row"> {childeren} </div>)
+    }
+    return boards
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createTable()}
       </div>
     );
   }
